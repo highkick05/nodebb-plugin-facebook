879b85d7e38dbdd6bf0d33b64d347333a97b14be
Matt Willis <mattwillis02@gmail.com>
2015-02-18 20:29:59 +0800

    Added: action:ajaxify.end listener
    FB.XFBML.parse() added to listener action:ajax.end event.
    Seem to fire the right event to load front page embed every time.
    Masonary not aligning correctly only bug left to fix, not sure why...

--- library.js
+++ library.js
@@ -2,10 +2,10 @@
     "use strict";
 
     var FacebookEmbed = {},
-        embed = '<script><!-- FB.XFBML.parse(facebookPosts);//--></script><div class="fb-post" data-href="https://www.facebook.com/$1/posts/$2" data-width="500"></div>';
-	var embedcode = /<a href="(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/(.+)?(?:\/posts\/)(.+)">.+<\/a>/g;
-
-    FacebookEmbed.parse = function(data, callback) {
+        embed = '<p class="fb-post" data-href="https://www.facebook.com/$1/posts/$2" data-width="395"</p>';
+		var embedcode = /<a href="(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/(.+)?(?:\/posts\/)(.+)">.+<\/a>/g
+	
+	FacebookEmbed.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
         }
@@ -14,7 +14,6 @@
         }
         callback(null, data);
 
-    };
-
+    };	
     module.exports = FacebookEmbed;
 }(module));
